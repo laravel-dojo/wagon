ca1e34caabaa45adc39e4345c9a47608fa96f43d
Shengyou Fan <shengyoufan@gmail.com>
2015-01-22 10:32:58 +0800

    update: setup uwamp and add laravel init app
    - setup apache port to 8000, load rewrite module, point to laravel/public
    - setup mysql port to 33060
    - add php.ini for cli
    - remove uwamp default www files

--- uwamp/phpapps/phpmyadmin/config.inc.php
+++ uwamp/phpapps/phpmyadmin/config.inc.php
@@ -29,6 +29,7 @@ $i++;
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
 $cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['port'] = '33060';
 $cfg['Servers'][$i]['connect_type'] = 'tcp';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
