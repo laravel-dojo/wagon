663cff616494a1f3d85ac3b30e0e384857b6cb0c
Shengyou Fan <shengyoufan@gmail.com>
2017-10-15 18:06:16 +0800

    fix build process
    - add 7z binary into repo (7z920_extra 32bits)
    - add installer.sfx from (https://github.com/alur/WinUnionFS)

--- build/build.cmd
+++ build/build.cmd
@@ -5,5 +5,5 @@ SET TARGET=wagon.7z
 SET TARGET_EXE=wagon.exe
 SET CONFIG=config.txt
 del /Q %TARGET% %TARGET_EXE%
-7za.exe a -t7z %TARGET% %SOURCE%
-copy /b 7zsd.sfx + %CONFIG% + %TARGET% %TARGET_EXE%
+7zr.exe a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on %TARGET% %SOURCE%
+copy /b installer.sfx + %CONFIG% + %TARGET% %TARGET_EXE%
