2efb154353637389cd7cea346211a79702d9b546
Shengyou Fan <shengyoufan@gmail.com>
2017-10-15 16:41:57 +0800

    add vc redist binary into repo and fix install command path

--- src/vc_redist/install.cmd
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-%~dp0\vc_redist_2012.x64.exe /passive /norestart
-%~dp0\vc_redist_2012.x86.exe /passive /norestart
-%~dp0\vc_redist_2015.x64.exe /passive /norestart
-%~dp0\vc_redist_2015.x86.exe /passive /norestart
---
+++ src/vcredist/install.cmd
@@ -0,0 +1,5 @@
+@ECHO OFF
+%~dp0\vcredist_2012_x64.exe /passive /norestart
+%~dp0\vcredist_2012_x86.exe /passive /norestart
+%~dp0\vcredist_2015_x64.exe /passive /norestart
+%~dp0\vcredist_2015_x86.exe /passive /norestart
