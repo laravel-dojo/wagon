d7806c6bbd3923e857402b03b203e18d2b6a306a
Shengyou Fan <shengyoufan@gmail.com>
2022-11-05 20:01:51 +0800

    upgrade vc redist to 2022

--- src/vcredist/install.cmd
+++ src/vcredist/install.cmd
@@ -1,5 +1,5 @@
 @ECHO OFF
 %~dp0\vcredist_2012_x64.exe /passive /norestart
 %~dp0\vcredist_2012_x86.exe /passive /norestart
-%~dp0\vcredist_2015-2019_x64.exe /passive /norestart
-%~dp0\vcredist_2015-2019_x86.exe /passive /norestart
+%~dp0\vcredist_2015-2022_x64.exe /passive /norestart
+%~dp0\vcredist_2015-2022_x86.exe /passive /norestart
